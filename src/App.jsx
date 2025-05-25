3a57d3c6aa59bf6a6786ebe223fb853d5c94fef8
Lucasvdam <lucascvandam@gmail.com>
2025-05-25 20:54:41 +0200

    initial working version

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1>Geopins 🌍</h1>
+      </header>
+      <main className="main">
+        <div className="map-placeholder">
+          Map will go here
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App 
